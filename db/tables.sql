5d7ad0d7973ed171bb1e49dfc034e9550dca5f9e
dxwc <dxwc@users.noreply.github.com>
2018-07-21 21:10:04 -0400

    changed id type from text to uuid
    and made submission's content required

--- db/tables.sql
+++ db/tables.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE people
 (
-    id     TEXT PRIMARY KEY, -- uuid v4
+    id     UUID PRIMARY KEY, -- uuid v4
     u_name TEXT NOT NULL UNIQUE,
     u_pass TEXT NOT NULL,
     joined INT NOT NULL, -- unix time
@@ -11,30 +11,30 @@ CREATE TABLE people
 
 CREATE TABLE submission
 (
-    id        TEXT PRIMARY KEY, -- uuid v4
-    posted_by TEXT REFERENCES people(id),
+    id        UUID PRIMARY KEY, -- uuid v4
+    posted_by UUID REFERENCES people(id),
     title     TEXT NOT NULL,
-    content   TEXT,
+    content   TEXT NOT NULL,
     published INT NOT NULL, -- unix time
     marker    SMALLINT -- markers such as reported, link post, text only post etc
 );
 
 CREATE TABLE comment
 (
-    id          TEXT PRIMARY KEY,
+    id          UUID PRIMARY KEY,
     parent_type BOOLEAN NOT NULL, -- true: post, false: comment
-    parent      TEXT NOT NULL,
-    posted_by   TEXT REFERENCES people(id),
+    parent      UUID REFERENCES submission(id),
+    posted_by   UUID REFERENCES people(id),
     content     TEXT NOT NULL,
     published   INT NOT NULL -- unix time
 );
 
 CREATE TABLE report
 (
-    id            TEXT PRIMARY KEY,
-    reported_id   TEXT NOT NULL, -- post or comment id
+    id            UUID PRIMARY KEY,
+    reported_id   UUID NOT NULL, -- post or comment id
     reported_type BOOLEAN NOT NULL, -- post or comment being reported
-    posted_by     TEXT REFERENCES people(id),
+    posted_by     UUID REFERENCES people(id),
     published     INT NOT NULL, -- for user
     content       TEXT NOT NULL, -- for user
     marker        SMALLINT, -- for user
