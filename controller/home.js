2f99d725a900ba0578be276276954770fc8510da
dxwc <dxwc@users.noreply.github.com>
2019-01-02 17:06:01 -0500

    basic home page view

--- controller/home.js
+++ controller/home.js
@@ -1,9 +1,37 @@
 let router = require('express').Router();
 let render = require('./function/render.js');
+let op     = require('../model/operations.js');
 
-router.get('/', (req, res) =>
+router.get('/', async (req, res) =>
 {
-    return render(req, res, 'home', null, true);
+    try
+    {
+        if
+        (
+            !global.front_page ||
+            (
+                typeof(global.front_page_time) === 'number' &&
+                (new Date().getTime() - global.front_page_time > 60000)
+            )
+        )
+        {
+
+            let temp = await op.front_page();
+            if(temp && temp.constructor === Array && temp.length)
+            {
+                global.front_page_time = new Date().getTime();
+               global.front_page = temp;
+               delete temp;
+            }
+        }
+
+        return render(req, res, 'home', { posts : global.front_page });
+    }
+    catch(err)
+    {
+        console.error(err);
+        return render(req, res, 'home');
+    }
 });
 
 module.exports = router;
