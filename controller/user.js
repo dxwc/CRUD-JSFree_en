54a638224c52a26981e192d080e09b528086804c
dxwc <dxwc@users.noreply.github.com>
2019-01-01 20:38:31 -0500

    show list of following user on self profile page

--- controller/user.js
+++ controller/user.js
@@ -7,12 +7,27 @@ router.get('/user/:user_name', async (req, res) =>
     try
     {
         let posts = await op.get_posts(req.params.user_name);
+
+        let following;
+        if
+        (
+            req.isAuthenticated() &&
+            req.session.passport.user.uname === req.params.user_name
+        )
+        {
+            following = await op.read_follows(req.session.passport.user.id);
+        }
+
         return render
         (
             req,
             res,
             'user',
-            { posts : posts, user_name : req.params.user_name }
+            {
+                posts     : posts,
+                following : following,
+                user_name : req.params.user_name
+            }
         );
     }
     catch(err)
