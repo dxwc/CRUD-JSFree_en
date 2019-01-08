2841a840e97025a7cb8edfff14d1a0f478d4ffc7
dxwc <dxwc@users.noreply.github.com>
2019-01-07 21:53:12 -0500

    added new posts link and newest post page
    todo: caching

--- controller/home.js
+++ controller/home.js
@@ -6,16 +6,44 @@ router.get('/', async (req, res) =>
 {
     try
     {
-        if
+        if(req.query.new)
+        {
+            if(req.query.page)
+            {
+                try
+                {
+                    req.query.page = Math.round(Number(req.query.page) - 1);
+                    if(req.query.page < 0) req.query.page = null;
+                }
+                catch(err)
+                {
+                    req.query.page = null;
+                }
+            }
+
+            if(req.query.page && typeof(req.query.page) === 'number')
+            {
+                temp = await op.front_page
+                (true, true, req.query.page * 100);
+
+                return render
+                (req, res, 'home', { posts :temp, page : req.query.page + 1 });
+            }
+            else
+            {
+                temp = await op.front_page(true, true);
+                return render(req, res, 'home', { posts :temp, page : 1 });
+            }
+        }
+        else if
         (
             !global.front_page ||
             (
                 typeof(global.front_page_time) === 'number' &&
-                (new Date().getTime() - global.front_page_time > 30000)
+                (new Date().getTime() - global.front_page_time > 60000)
             )
         )
         {
-
             let temp = await op.front_page();
             if(temp && temp.constructor === Array && temp.length)
             {
@@ -23,9 +51,13 @@ router.get('/', async (req, res) =>
                global.front_page = temp;
                delete temp;
             }
-        }
 
-        return render(req, res, 'home', { posts : global.front_page });
+            return render(req, res, 'home', { posts : global.front_page });
+        }
+        else
+        {
+            return render(req, res, 'home', { posts : global.front_page });
+        }
     }
     catch(err)
     {
